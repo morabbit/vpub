3c73f828cc586b85a5147ed534ba6a83d7eb07d1
heyu330182 <heyu330182@hotmail.com>
2024-05-29 14:41:23 +0800

    Update trial.sh
    supporting share

--- trial.sh
+++ trial.sh
@@ -1,30 +1,89 @@
 #!/bin/bash
 
-time=$(date "+%Y/%m/%Y%m%d")
+time=$(date "+%Y%m")
+num="1"
 file=".txt"
-path="https://clashnode.com/wp-content/uploads/"
+path_share="https://raw.staticdn.net/mianfeifq/share/main/data"
+path_pawdroid="https://raw.staticdn.net/Pawdroid/Free-servers/main/sub"
+pf="privateused"
 
-FILEURL=${path}${time}${file}
+function clearold(){
+    >$pf
+}
+
+function pawdroid(){
+echo "START PAWDROID"
+FILEURL=${path_pawdroid}
 echo ${FILEURL}
 
->privateused
+RESULT=$(curl -L -XGET -o /dev/null -w " %{http_code}" ${FILEURL})
+echo $RESULT
 
-RESULT=$(curl -XGET ${FILEURL})
-echo $RESULT>>privateused
+# 检查curl命令是否成功
+if [ $RESULT -eq 200 ]; then
+  echo "pawdroid内容已成功写入"
+  echo $(curl -L -XGET ${FILEURL})>>$pf
+else
+  echo "pawdroid获取内容失败: $RESULT"
+fi
+echo "END PAWDROID"
+}
 
-fileyaml=".yaml"
-pathyaml="https://clashnode.com/wp-content/uploads/"
-FILEURLYAML=${pathyaml}${time}${fileyaml}
-echo ${FILEURLYAML}
+function share(){
+echo "START SHARE"
+for num in {1..31}
+do
+    FILEURL=${path_share}${time}${num}${file}
+    echo ${FILEURL}
+
+    RESULT=$(curl -L -XGET -o /dev/null -w " %{http_code}" ${FILEURL})
+    echo $RESULT
+
+    # 检查curl命令是否成功
+    if [ $RESULT -eq 200 ]; then
+        clearold
+        echo $(curl -L -XGET ${FILEURL})>>$pf
+        echo "share内容已成功写入"
+        return 0
+    else
+        echo "share获取内容失败: $RESULT"
+    fi
+
+done
+echo "END SHARE"
+return 0
+}
+
+
+share
+#pawdroid
+
+>README.md
+echo "update on" $(date "+%Y-%m-%d %H:%M:%S") >  README.md
+
+
+
+
+#########################################
+
+
+#echo $(curl -L -XGET ${path1})>>privateused
+
+#wget ${FILEURL} | xargs >> privateused
+#curl -L -o privateused ${FILEURL}
+
+#fileyaml=".yaml"
+#pathyaml="https://clashnode.com/wp-content/uploads/"
+#FILEURLYAML=${pathyaml}${time}${fileyaml}
+#echo ${FILEURLYAML}
 
 #>privateused.yaml
 #RESULTYAML=$(curl -XGET ${FILEURLYAML})
 #echo $RESULTYAML>>privateused.yaml
 
-curl -XGET ${FILEURLYAML} -O
-dateyaml=$(date "+%Y%m%d")
-getyaml=${dateyaml}${fileyaml}
-mv ${getyaml} privateused.yaml
+#curl -XGET ${FILEURLYAML} -O
+#dateyaml=$(date "+%Y%m%d")
+#getyaml=${dateyaml}${fileyaml}
+#mv ${getyaml} privateused.yaml
 
->README.md
-echo "update on" ${time} >  README.md
+
