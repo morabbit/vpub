64785ce1bd0bedf1776609928ecf25743d56c083
heyu330182 <heyu330182@hotmail.com>
2024-07-25 17:43:08 +0800

    Update trial.sh
    修改trial

--- trial.sh
+++ trial.sh
@@ -5,6 +5,11 @@ num="1"
 file=".txt"
 path_share="https://raw.staticdn.net/mianfeifq/share/main/data"
 path_pawdroid="https://raw.staticdn.net/Pawdroid/Free-servers/main/sub"
+path_huibq="https://raw.staticdn.net/Huibq/TrojanLinks/master/links"
+huibq_trojan="/trojan"
+huibq_vmess="/vmess"
+huibq_ssr="/ssr"
+
 pf="privateused"
 newone=
 
@@ -32,7 +37,7 @@ echo "END PAWDROID"
 
 function share(){
 echo "START SHARE"
-for num in {0..31}
+for num in {1..31}
 do
     FILEURL=${path_share}${time}${num}${file}
     echo ${FILEURL}
@@ -55,6 +60,60 @@ done
 echo "END SHARE"
 return -1 
 }
+
+function huibq(){
+echo "START huibq"
+
+FILEURL1=${path_huibq}${huibq_trojan}
+FILEURL2=${path_huibq}${huibq_vmess}
+FILEURL3=${path_huibq}${huibq_ssr}
+echo ${FILEURL1}
+echo ${FILEURL2}
+echo ${FILEURL3}
+
+RESULT=$(curl -L -XGET -o /dev/null -w " %{http_code}" ${FILEURL1})
+echo $RESULT
+
+# 检查curl命令是否成功
+if [ $RESULT -eq 200 ]; then
+    clearold
+    echo $(curl -L -XGET ${FILEURL1})>>$pf
+    echo "huibq trojan 内容已成功写入"
+    return 0
+else
+    echo "buibq trojan 获取内容失败: $RESULT"
+fi
+
+RESULT=$(curl -L -XGET -o /dev/null -w " %{http_code}" ${FILEURL2})
+echo $RESULT
+
+# 检查curl命令是否成功
+if [ $RESULT -eq 200 ]; then
+    clearold
+    echo $(curl -L -XGET ${FILEURL2})>>$pf
+    echo "huibq vmess 内容已成功写入"
+    return 0
+else
+    echo "buibq vmess 获取内容失败: $RESULT"
+fi
+
+RESULT=$(curl -L -XGET -o /dev/null -w " %{http_code}" ${FILEURL3})
+echo $RESULT
+
+# 检查curl命令是否成功
+if [ $RESULT -eq 200 ]; then
+    clearold
+    echo $(curl -L -XGET ${FILEURL3})>>$pf
+    echo "huibq ssr 内容已成功写入"
+    return 0
+else
+    echo "buibq ssr 获取内容失败: $RESULT"
+fi
+
+echo "END huibq"
+return -1 
+}
+
 
 function checknewone(){
 if [ ! $newone ];then
@@ -86,9 +145,10 @@ do
 done
 }
 
-share
-checknewone
+#share
+#checknewone
 #pawdroid
+huibq
 
 >README.md
 echo "update on" $(date "+%Y-%m-%d %H:%M:%S") >  README.md
